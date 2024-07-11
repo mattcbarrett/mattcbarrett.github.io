4d9ded5752cd7ce1314f5157d79ce8005cc1a252
mattcbarrett <mattcbarrett@gmail.com>
2024-07-10 21:34:21 -0700

    spacing between elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-x-1 items-center justify-center min-h-screen bg-zinc-900">
+    <div className="flex flex-col space-y-4 items-center justify-center min-h-screen bg-zinc-900">
       <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
         sup
       </div>
