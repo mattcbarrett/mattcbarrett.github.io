292c1dc4d388accf652583b37416cb0d94084c7d
mattcbarrett <mattcbarrett@gmail.com>
2024-07-10 21:46:09 -0700

    layout testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-4 items-center justify-center min-h-screen bg-zinc-900">
-      <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-        sup
+    <div className="flex space-x-8 items-center">
+      <div className="flex flex-col space-y-4 items-center justify-center min-h-screen bg-zinc-900">
+        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
+          sup
+        </div>
+        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
+          sup?
+        </div>
+        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
+          sup.
+        </div>
       </div>
       <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-        sup?
-      </div>
-      <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-        sup.
+        about
       </div>
     </div>
   );
