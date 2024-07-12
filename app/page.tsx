15920fd7a5bbef25815af490913e325528f6d88f
mattcbarrett <mattcbarrett@gmail.com>
2024-07-11 22:07:42 -0700

    layout update

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex space-x-8 items-center">
-      <div className="flex flex-col space-y-4 items-center justify-center min-h-screen bg-zinc-900">
-        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-          sup
+    <div className="flex space-x-8 items-center justify-center bg-zinc-900">
+      <div className="w-4/5 mx-auto">
+        <div className="flex items-center space-x-8 justify-center">
+          <div className="flex flex-col w-3/5 space-y-4 items-center justify-start min-h-screen ">
+            <div className="p-8 rounded-lg shadow-lg w-full text-center bg-zinc-800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <br />
+            <br />
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+            </div>
+            <div className="p-8 rounded-lg shadow-lg w-full text-center bg-zinc-800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div className="p-8 rounded-lg shadow-lg w-full text-center bg-zinc-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+          <div className="flex flex-col w-1/5 space-y-4 items-center justify-start min-h-screen ">
+            <div className="p-8 rounded-lg shadow-lg w-full text-center bg-zinc-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
         </div>
-        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-          sup?
-        </div>
-        <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-          sup.
-        </div>
-      </div>
-      <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
-        about
       </div>
     </div>
   );
