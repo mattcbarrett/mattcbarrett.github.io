7f0ce8308095f91c2e4b41194d91269284d691d6
mattcbarrett <mattcbarrett@gmail.com>
2024-08-05 22:11:17 -0700

    Move top nav to layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex min-h-12 justify-center items-center bg-zinc-900">
+          <div className="w-4/5 space-x-4 space-y-4 text-center">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/projects"}>Projects</Link>
+            <Link href={"/about"}>About</Link>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
