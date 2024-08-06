09baff6bf1c1c6230d79973284b43b181378b25c
mattcbarrett <mattcbarrett@gmail.com>
2024-08-05 22:13:53 -0700

    initial commit

---
+++ lib/utilities.ts
@@ -0,0 +1,27 @@
+import fs from "fs"
+import matter from "gray-matter"
+import { join } from "path"
+import { Post } from "@/lib/types"
+
+const contentDir = join(process.cwd(), "content")
+
+export const getFilenames = () => {
+  return fs.readdirSync(contentDir)
+}
+
+export const getPostFromFile = (filename: string) => {
+  const slug = filename.replace(/\.md$/, "")
+  const fullPath = join(contentDir, filename)
+  const fileContents = fs.readFileSync(fullPath, "utf-8")
+  const { data, content } = matter(fileContents)
+
+  return { ...data, slug, content } as Post
+}
+
+export const getAllPosts = () => {
+  const files = getFilenames()
+  const posts = files
+  .map((filename) => getPostFromFile(filename))
+  .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+  return posts
+}
