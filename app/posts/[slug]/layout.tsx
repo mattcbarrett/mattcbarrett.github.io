573b629c5f04c1f95dfd99d28cf7c1365b913281
mattcbarrett <mattcbarrett@gmail.com>
2024-08-08 21:28:06 -0700

    responsive layout tweaks

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -6,9 +6,9 @@ export default function PostLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <div className="flex space-x-8 items-center justify-center bg-zinc-900">
-      <div className="w-4/5 mx-auto">
-        <div className="flex space-x-8 justify-center">
-          <div className="flex flex-col w-3/5 space-y-4 items-center justify-start min-h-screen">
+      <div className="lg:w-4/5 md:w-full mx-auto">
+        <div className="flex flex-col lg:flex-row lg:space-x-8 justify-center">
+          <div className="flex flex-col w-full lg:w-3/5 space-y-4 items-center justify-start mb-4">
             <div className="p-8 space-y-4 rounded-lg shadow-lg w-full bg-zinc-800">
               <div className="text-justify p-x-4 space-y-4">
                 {children}
