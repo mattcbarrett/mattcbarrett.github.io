c6f9a60ba6c0b713d608889aa6fdf34b342732a4
mattcbarrett <mattcbarrett@gmail.com>
2024-09-06 07:00:03 -0700

    Layout tweaks

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,7 +4,6 @@ type Props = {}
 
 const Sidebar = (props: Props) => {
   return (
-    <div className="flex flex-col w-full mb-4 lg:w-1/5">
       <div className="p-8 space-y-2 rounded-lg shadow-lg w-full text-left bg-zinc-800">
         <p className="mb-4">Thoughts on information security, code & automation, and other myriad interests of mine.</p>
         <div className="flex space-x-2">
@@ -14,7 +13,6 @@ const Sidebar = (props: Props) => {
           <FaLinkedin className="self-center"/><a href="https://www.linkedin.com/in/matt-barrett-7bb48815b/">LinkedIn</a>
         </div>
       </div>
-    </div>
   )
 }
 
