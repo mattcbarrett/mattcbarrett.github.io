39b3b2e1a37eb61b7d5ff96d41f7f4faed6a373d
mattcbarrett <mattcbarrett@gmail.com>
2024-09-07 07:36:26 -0700

    Fix nested <Link> components

--- app/components/BlogPostTeaser.tsx
+++ app/components/BlogPostTeaser.tsx
@@ -11,9 +11,9 @@ const BlogPostTeaser = (props: Props) => {
   const teaser = post.content.substring(0,350) + "..." //ellipsis so the post's content trails off
   
   return (
-    <div className="p-8 rounded-lg shadow-lg w-full bg-zinc-800">
+    <div className="p-8 rounded-lg shadow-lg w-full bg-zinc-800 hover:bg-zinc-700">
       <Link href={`/posts/${post.slug}`} className="space-y-4">
-        <div className="flex justify-between hover:text-zinc-300">
+        <div className="flex justify-between">
           <div className="text-xl">
             {post.title}
           </div>
@@ -21,24 +21,22 @@ const BlogPostTeaser = (props: Props) => {
             {post.date}
           </div>
         </div>
-        <div className="p-x-4 hover:text-zinc-300">
-          <Markdown className="prose text-left max-w-fit text-zinc-400 hover:text-zinc-300">{teaser}</Markdown>
+        <div className="p-x-4">
+          <Markdown className="prose text-left max-w-fit text-zinc-400">{teaser}</Markdown>
         </div>
-        <div className="flex justify-between">
-          <div className="text-sm">
-            {post.tags.map((tag, index) => (
-              <Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`} className="hover:text-zinc-300">
-                {`${tag}, `}
-              </Link>
-            ))}
-          </div>
-
-          <div className="text-right text-sm">
-            -{post.author}
-          </div>
-        </div>
-        
       </Link>
+      <div className="mt-8 flex justify-between">
+        <div className="text-sm">
+          {post.tags.map((tag, index) => (
+            <Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`} className="hover:text-zinc-300" key={`link-${index}`}>
+              {`${tag}, `}
+            </Link>
+          ))}
+        </div>
+        <div className="text-right text-sm">
+          -{post.author}
+        </div>
+      </div>
     </div>
   )
 }
