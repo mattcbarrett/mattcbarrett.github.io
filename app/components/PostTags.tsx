8e1739c8b657fd925272ed6ea9e63aba576dd824
mattcbarrett <mattcbarrett@gmail.com>
2024-09-07 10:25:51 -0700

    Add css for hover

--- app/components/PostTags.tsx
+++ app/components/PostTags.tsx
@@ -11,7 +11,7 @@ const PostTags = ({ tags, tagsLower }: Props) => {
     <div className="p-8 space-y-2 rounded-lg shadow-lg w-full text-left bg-zinc-800">
       <ul key="list">
         {dedupedTags.map((tag, index) => (
-          <li key={index}><Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`}>{tag}</Link></li>
+          <li key={index}><Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`} className="hover:text-zinc-300">{tag}</Link></li>
         ))}
       </ul>
     </div>
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -7,10 +7,10 @@ const Sidebar = (props: Props) => {
       <div className="p-8 space-y-2 rounded-lg shadow-lg w-full text-left bg-zinc-800">
         <p className="mb-4">Thoughts on information security, code & automation, and other myriad interests of mine.</p>
         <div className="flex space-x-2">
-          <FaGithub className="self-center"/><a href="https://github.com/mattcbarrett">GitHub</a>
+          <FaGithub className="self-center"/><a href="https://github.com/mattcbarrett" className="hover:text-zinc-300">GitHub</a>
         </div>
         <div className="flex space-x-2">
-          <FaLinkedin className="self-center"/><a href="https://www.linkedin.com/in/matt-barrett-7bb48815b/">LinkedIn</a>
+          <FaLinkedin className="self-center"/><a href="https://www.linkedin.com/in/matt-barrett-7bb48815b/" className="hover:text-zinc-300">LinkedIn</a>
         </div>
       </div>
   )
