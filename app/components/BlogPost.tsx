45c5bb8d064c6d47ea3c9123d3a125b9369063f4
mattcbarrett <mattcbarrett@gmail.com>
2024-10-03 20:28:14 -0700

    Move post date under title

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -8,22 +8,20 @@ type Props = {
 const BlogPost = (props: Props) => {
   const { post } = props
   return (
-    <>
-      <div className="flex justify-between"> 
-        <div className="text-xl">
-          {post.title}
-        </div>
-        <div className="text-sm self-center">
-          {post.date}
-        </div>
+    <div className="space-y-2">
+      <div className="text-xl font-bold">
+        {post.title}
+      </div>
+      <div className="text-sm self-center">
+        {post.date}
       </div>
       <div className="my-4">
-        <Markdown className="prose text-left max-w-fit text-zinc-400">{post.content}</Markdown>
+        <Markdown className="prose prose-zinc400 text-left max-w-fit">{post.content}</Markdown>
       </div>
-      <div className="text-right text-sm mt-8">
+      <div className="text-right text-sm pt-8">
         -{post.author}
       </div>
-    </>
+    </div>
   )
 }
 
