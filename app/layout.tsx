472fabcc49887ec1413804d7d357ed06bb1867ea
mattcbarrett <mattcbarrett@gmail.com>
2024-10-03 20:25:56 -0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="5838b6ce-2920-4094-966d-f87a43bb57d4" />
       </head>
       <body className={`${inter.className} bg-zinc-900 text-zinc-400`}>
         <div className="flex min-h-4 justify-center items-center">
