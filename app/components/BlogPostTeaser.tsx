7b6b484bea21d55b0866b331154adb01ac7fae69
mattcbarrett <mattcbarrett@gmail.com>
2024-10-03 20:29:12 -0700

    Move post date under title. Fix post text color.

--- app/components/BlogPostTeaser.tsx
+++ app/components/BlogPostTeaser.tsx
@@ -13,30 +13,31 @@ const BlogPostTeaser = (props: Props) => {
 
   return (
     <div className="p-8 rounded-lg shadow-lg w-full bg-zinc-800 hover:bg-zinc-700">
-      <Link href={`/posts/${post.slug}`} className="space-y-4">
-        <div className="flex justify-between">
-          <div className="text-xl">
-            {post.title}
-          </div>
-          <div className="text-sm self-center">
-            {post.date}
-          </div>
+      <Link href={`/posts/${post.slug}`} className="space-y-2">
+        <div className="text-xl font-bold">
+          {post.title}
+        </div>
+        <div className="text-sm self-center">
+          {post.date}
         </div>
         <div className="p-x-4">
-          <Markdown className="prose text-left max-w-fit text-zinc-400">{teaser}</Markdown>
+          <Markdown className="prose prose-zinc400 text-left max-w-fit text-zinc-400">{teaser}</Markdown>
         </div>
       </Link>
       <div className="mt-8 flex justify-between">
-        <div className="text-sm">
-          {post.tags.map((tag, index) => (
-            <Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`} className="hover:text-zinc-300" key={`link-${index}`}>
-              {index === (post.tags.length - 1) ? `${tag}` : `${tag}, `}
-            </Link>
-          ))}
+        <div className="flex">
+          <FaTags className="self-center mr-2" />
+          <div className="text-sm">  
+            {post.tags.map((tag, index) => (
+              <Link href={`/tags/${tag.toLowerCase().replaceAll(" ", "-")}`} className="hover:text-zinc-300" key={`link-${index}`}>
+                {index === (post.tags.length - 1) ? `${tag}` : `${tag}, `}
+              </Link>
+            ))}
+          </div>
         </div>
-        <div className="text-right text-sm">
+        {/* <div className="text-right text-sm">
           -{post.author}
-        </div>
+        </div> */}
       </div>
     </div>
   )
