24a4f372b5a6fb6aee7ca073a31700b9475cf768
mattcbarrett <mattcbarrett@gmail.com>
2024-10-11 07:15:29 -0700

    Resources page

--- app/components/MenuCard.tsx
+++ app/components/MenuCard.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Link from 'next/link'
-import { FaGithub, FaLinkedin, FaHome, FaUser } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaHome, FaUser, FaToolbox } from "react-icons/fa"
 
 type Props = {}
 
@@ -11,6 +11,9 @@ const MenuCard = (props: Props) => {
         <p className="mb-4 pr-8">Thoughts on information security, code & automation, and other myriad interests of mine.</p>
         <div className="flex space-x-2 hover:text-lime-400">
           <FaHome className="self-center" /><Link href="/">Home</Link>
+        </div>
+        <div className="flex space-x-2 hover:text-lime-400">
+          <FaToolbox className="self-center" /><Link href="/resources">Resources</Link>
         </div>
         <div className="flex space-x-2 hover:text-lime-400">
           <FaUser className="self-center" /><Link href="/about">About</Link>
---
+++ app/resources/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import { getAllPosts } from '@/lib/utilities'
+import Link from 'next/link'
+
+type Props = {}
+
+const AboutPage = (props: Props) => {
+  const allPosts = getAllPosts()
+  const tags = 
+    allPosts
+      .map(post => post.tags)
+      .flat()
+      .sort()
+
+  const tagsLower = 
+    allPosts
+      .map(post => post.tagsLower)
+      .flat()
+      .sort()
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="lg:w-4/5 md:w-full px-8 mx-auto">
+        <div className="flex flex-col lg:flex-row lg:space-x-8 justify-center">
+          {/* Move Sidebar component after 3/5th width div to place sidebar on right side of page. Then change parent div to flex-col-reverse to ensure menu button appears at top of page on small viewports. */}
+          <Sidebar tags={tags} tagsLower={tagsLower} />   
+          <div className="flex flex-col w-full lg:w-3/5 py-8 items-center mb-4">
+            <p className="prose prose-zinc400 text-left max-w-fit">
+              {"Links to useful stuff."}
+            </p>
+            <Link href="https://virustotal.com" className="hover:text-lime-400">Virustotal</Link>
+            <Link href="https://p.ost2.fyi/courses/" className="hover:text-lime-400">OpenSecurityTraining2</Link>
+            <Link href="https://malwaretech.com/" className="hover:text-lime-400">MalwareTech</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
