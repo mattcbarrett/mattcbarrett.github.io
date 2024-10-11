e79e6b22e1f5dc018b9a8f5a9ee055cba9aeef22
mattcbarrett <mattcbarrett@gmail.com>
2024-10-10 22:07:08 -0700

    forgot about the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -19,17 +19,15 @@ const AboutPage = (props: Props) => {
       .sort()
 
   return (
-    <div className="flex space-x-8 items-center justify-center">
-      <div className="lg:w-4/5 md:w-full mx-auto">
+    <div className="flex items-center justify-center">
+      <div className="lg:w-4/5 md:w-full px-8 mx-auto">
         <div className="flex flex-col lg:flex-row lg:space-x-8 justify-center">
           {/* Move Sidebar component after 3/5th width div to place sidebar on right side of page. Then change parent div to flex-col-reverse to ensure menu button appears at top of page on small viewports. */}
           <Sidebar tags={tags} tagsLower={tagsLower} />   
-          <div className="flex flex-col w-full lg:w-3/5 space-y-4 items-center mb-4">
-            <div className="p-8 rounded-lg shadow-lg w-full bg-zinc-800">
-              <p className="prose prose-zinc400 text-left max-w-fit">
-                {"I've learned a great deal from others over the years, benefitting from the idea that knowledge should be free. This is my effort to practice that same philosophy, contribute to the community that's helped me succeed, and pass on my own knowledge so others may benefit in the same way."}
-              </p>
-            </div>
+          <div className="flex flex-col w-full lg:w-3/5 py-8 items-center mb-4">
+            <p className="prose prose-zinc400 text-left max-w-fit">
+              {"I've learned a great deal from others over the years, benefitting from the idea that knowledge should be free. This is my effort to practice that same philosophy, contribute to the community that's helped me succeed, and pass on my own knowledge so others may benefit in the same way."}
+            </p>
           </div>
         </div>
       </div>
