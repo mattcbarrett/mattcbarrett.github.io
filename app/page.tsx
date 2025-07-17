c7dccc1cacc2e3b299435902f4d6fe26e9c0e19f
mattcbarrett <mattcbarrett@gmail.com>
2025-07-17 07:07:54 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       {posts.map((post, index) => (
           <BlogPostTeaser post={post} key={post.slug} />
       ))}
-      <div className="text-center space-x-2 mb-8">
+      <div className="text-center space-x-2 mb-4">
         {paginatedPosts.map((page, index) => (
           <Link href={`/page/${index + 1}`} key={`page-${index}`} className="hover:text-lime-400">{index + 1}</Link>
         ))}
